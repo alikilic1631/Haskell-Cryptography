fd9e2c45aa3326a80e53935a24210d75aba0eb8e
Ali Kilic <ak5323@ash07.doc.ic.ac.uk>
2023-10-09 18:14:38 +0100

    first draft

---
+++ src/Crypto.hs
@@ -0,0 +1,135 @@
+module Crypto ( gcd, smallestCoPrimeOf, phi, computeCoeffs, inverse
+              , modPow, genKeys, rsaEncrypt, rsaDecrypt, toInt, toChar
+              , add, subtract, ecbEncrypt, ecbDecrypt
+              , cbcEncrypt, cbcDecrypt ) where
+
+import Data.Char
+
+import Prelude hiding (gcd, subtract)
+
+{-
+The advantage of symmetric encryption schemes like AES is that they are efficient
+and we can encrypt data of arbitrary size. The problem is how to share the key.
+The flaw of the RSA is that it is slow and we can only encrypt data of size lower
+than the RSA modulus n, usually around 1024 bits (64 bits for this exercise!).
+
+We usually encrypt messages with a private encryption scheme like AES-256 with
+a symmetric key k. The key k of fixed size 256 bits for example is then exchanged
+via the aymmetric RSA.
+-}
+
+-------------------------------------------------------------------------------
+-- PART 1 : asymmetric encryption
+
+-- | Returns the greatest common divisor of its two arguments
+gcd :: Int -> Int -> Int
+gcd m n
+  | n==0      = m
+  | otherwise = gcd n (m`mod`n) 
+
+
+-- | Euler Totient function
+phi :: Int -> Int
+phi m = lenght[x | x <- [1..m], gcd m x == 1]
+
+{-|
+Calculates (u, v, d) the gcd (d) and Bezout coefficients (u and v)
+such that au + bv = d
+-}
+computeCoeffs :: Int -> Int -> (Int, Int)
+computeCoeffs a b 
+  | q
+  |
+  where 
+    q= a`div`b
+    r= a`mod`b
+
+
+-- | Inverse of a modulo m
+inverse :: Int -> Int -> Int
+inverse = undefined
+
+-- | Calculates (a^k mod m)
+modPow :: Int -> Int -> Int -> Int
+modPow a k m
+  | k`mod`2 == 0 = ((a^2)`mod`m)^j`mod`m
+  | otherwise    = (a*((((a^2)`mod`m)^j)`mod`m))`mod`m
+  where 
+    j = k`div`2
+
+
+
+-- | Returns the smallest integer that is coprime with phi
+-- | Helper function is called "incrementer". 
+-- |It increments a arbitrary variable "b" and uses recursion to increase b by one.
+smallestCoPrimeOf :: Int -> Int
+smallestCoPrimeOf a = incrementer a 2
+  where incrementer :: Int -> Int -> Int 
+  incrementer a b
+    | gcd a b == 1 = b
+    | otherwise    = incrementer a (b+1)
+     
+
+{-|
+Generates keys pairs (public, private) = ((e, n), (d, n))
+given two "large" distinct primes, p and q
+-}
+genKeys :: Int -> Int -> ((Int, Int), (Int, Int))
+genKeys = undefined
+
+-- | This function performs RSA encryption
+rsaEncrypt :: Int        -- ^ value to encrypt
+           -> (Int, Int) -- ^ public key
+           -> Int
+rsaEncrypt = undefined
+
+-- | This function performs RSA decryption
+rsaDecrypt :: Int        -- ^ value to decrypt
+           -> (Int, Int) -- ^ public key
+           -> Int
+rsaDecrypt = undefined
+
+-------------------------------------------------------------------------------
+-- PART 2 : symmetric encryption
+
+-- | Returns position of a letter in the alphabet
+toInt :: Char -> Int
+toInt = undefined
+
+-- | Returns the n^th letter
+toChar :: Int -> Char
+toChar = undefined
+
+-- | "adds" two letters
+add :: Char -> Char -> Char
+add = undefined
+
+-- | "subtracts" two letters
+subtract :: Char -> Char -> Char
+subtract = undefined
+
+-- the next functions present
+-- 2 modes of operation for block ciphers : ECB and CBC
+-- based on a symmetric encryption function e/d such as "add"
+
+-- | ecb (electronic codebook) encryption with block size of a letter
+ecbEncrypt :: Char -> [Char] -> [Char]
+ecbEncrypt = undefined
+
+-- | ecb (electronic codebook) decryption with a block size of a letter
+ecbDecrypt :: Char -> [Char] -> [Char]
+ecbDecrypt = undefined
+
+-- | cbc (cipherblock chaining) encryption with block size of a letter
+cbcEncrypt :: Char   -- ^ public key
+           -> Char   -- ^ initialisation vector `iv`
+           -> [Char] -- ^ message `m`
+           -> [Char]
+cbcEncrypt = undefined
+
+-- | cbc (cipherblock chaining) decryption with block size of a letter
+cbcDecrypt :: Char   -- ^ private key
+           -> Char   -- ^ initialisation vector `iv`
+           -> [Char] -- ^ message `m`
+           -> [Char]
+cbcDecrypt = undefined
