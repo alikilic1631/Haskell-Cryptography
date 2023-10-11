7ce018a63256d3683c51178377729682cd50fee7
Ali Kilic <ak5323@ash07.doc.ic.ac.uk>
2023-10-11 19:10:10 +0100

    second_attempt

--- src/Crypto.hs
+++ src/Crypto.hs
@@ -24,89 +24,108 @@ via the aymmetric RSA.
 -- | Returns the greatest common divisor of its two arguments
 gcd :: Int -> Int -> Int
 gcd m n
-  | n==0      = m
+  | n == 0    = m
   | otherwise = gcd n (m`mod`n) 
 
 
 -- | Euler Totient function
 phi :: Int -> Int
-phi m = lenght[x | x <- [1..m], gcd m x == 1]
+phi m = length[x | x <- [1..m], gcd m x == 1]
 
 {-|
 Calculates (u, v, d) the gcd (d) and Bezout coefficients (u and v)
 such that au + bv = d
 -}
 computeCoeffs :: Int -> Int -> (Int, Int)
-computeCoeffs a b 
-  | q
-  |
-  where 
-    q= a`div`b
-    r= a`mod`b
+computeCoeffs a b  
+  let  
+    q = a`div`b
+    r     = a`mod`b
+    (u,v) = computeCoeffs b r
+  in
+    | b == 0    = (1,0)
+    | otherwise = (v,(u-(q*v))
+
+  
 
 
 -- | Inverse of a modulo m
 inverse :: Int -> Int -> Int
-inverse = undefined
+inverse a m = u`mod`m
+  where 
+    (u,v) = computeCoeffs a m
+
+  
 
 -- | Calculates (a^k mod m)
 modPow :: Int -> Int -> Int -> Int
 modPow a k m
-  | k`mod`2 == 0 = ((a^2)`mod`m)^j`mod`m
-  | otherwise    = (a*((((a^2)`mod`m)^j)`mod`m))`mod`m
-  where 
+  | k == 0    = 1`mod`m
+  | even k    = (modPow a_new j m)`mod`m 
+  | otherwise = a*(modPow a_new (k-1) m)`mod`m
+  where
     j = k`div`2
+    a_new = (a*a)`mod`m
 
 
 
 -- | Returns the smallest integer that is coprime with phi
--- | Helper function is called "incrementer". 
+-- | Helper function is called "helper_function". 
 -- |It increments a arbitrary variable "b" and uses recursion to increase b by one.
 smallestCoPrimeOf :: Int -> Int
-smallestCoPrimeOf a = incrementer a 2
-  where incrementer :: Int -> Int -> Int 
-  incrementer a b
-    | gcd a b == 1 = b
-    | otherwise    = incrementer a (b+1)
-     
+smallestCoPrimeOf a = helper_function a 2
+  where helper_function :: Int -> Int -> Int
+        helper_function a b 
+          | gcd a b == 1 = b
+          | otherwise    = helper_function a (b+1)
+      
 
 {-|
 Generates keys pairs (public, private) = ((e, n), (d, n))
 given two "large" distinct primes, p and q
 -}
 genKeys :: Int -> Int -> ((Int, Int), (Int, Int))
-genKeys = undefined
+genKeys p q = ((e,N),(d,N))
+  where 
+    N = p*q
+    product = (p-1)*(q-1)
+    e = smallestCoPrimeOf(product)
+    d = (1`mod`(product))`div`e
+    
+  
 
 -- | This function performs RSA encryption
 rsaEncrypt :: Int        -- ^ value to encrypt
            -> (Int, Int) -- ^ public key
            -> Int
-rsaEncrypt = undefined
+rsaEncrypt x (e,N) = modPow x e n
 
 -- | This function performs RSA decryption
 rsaDecrypt :: Int        -- ^ value to decrypt
            -> (Int, Int) -- ^ public key
            -> Int
-rsaDecrypt = undefined
+rsaDecrypt c (d,N) = modPow c d N
 
 -------------------------------------------------------------------------------
 -- PART 2 : symmetric encryption
 
 -- | Returns position of a letter in the alphabet
 toInt :: Char -> Int
-toInt = undefined
+toInt a = ord(a)-97
 
 -- | Returns the n^th letter
 toChar :: Int -> Char
-toChar = undefined
+toChar a = chr(a+97)
 
 -- | "adds" two letters
 add :: Char -> Char -> Char
-add = undefined
-
+add a b = toChar((toInt a + toInt b)`mod`26)
+  
 -- | "subtracts" two letters
 subtract :: Char -> Char -> Char
-subtract = undefined
+subtract a b 
+  | toInt a > toInt b = toChar((toInt a) - (toInt b)`mod`26)
+  | otherwise         = toChar((toInt b) - (toInt a)`mod`26)
 
 -- the next functions present
 -- 2 modes of operation for block ciphers : ECB and CBC
@@ -114,11 +133,13 @@ subtract = undefined
 
 -- | ecb (electronic codebook) encryption with block size of a letter
 ecbEncrypt :: Char -> [Char] -> [Char]
-ecbEncrypt = undefined
+ecbEncrypt k [] = []
+ecbEncrypt k c:cs = (add k c): ecbEncrypt k cs
 
 -- | ecb (electronic codebook) decryption with a block size of a letter
 ecbDecrypt :: Char -> [Char] -> [Char]
-ecbDecrypt = undefined
+ecbDecrypt k [] = []
+ecbDecrypt k c:cs = (subtract k c): ecbDecrypt k cs
 
 -- | cbc (cipherblock chaining) encryption with block size of a letter
 cbcEncrypt :: Char   -- ^ public key
